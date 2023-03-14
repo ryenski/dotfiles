231a850b0b5ebfa48099639c003a540c9fb87709
Ryan Crispin Heneise <ryan@boosterstage.net>
2023-03-14 11:54:18 -0500

    Up and down search history

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -11,3 +11,10 @@ bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
 bindkey "^Q" push-line-or-edit
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
